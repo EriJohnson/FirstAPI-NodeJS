8e51bda9885fd0e79013450060ed77cc37fd994c
Eri JS Fernandes <erijohnsons.fernandes@gmail.com>
2021-07-18 09:36:14 -0300

    upload do projeto

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const url = require('url');
+
+const routes = require('./routes');
+const bodyParser = require('./helpers/bodyParser');
+
+const server = http.createServer((request, response) => {
+  const parsedUrl = url.parse(request.url, true);
+
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndPoint = pathname.split('/').filter(Boolean);
+
+  if (splitEndPoint.length > 1) {
+    pathname = `/${splitEndPoint[0]}/:id`;
+
+    id = splitEndPoint[1];
+  }
+
+  const route = routes.find(
+    routeObj =>
+      routeObj.endpoint === pathname && routeObj.method === request.method
+  );
+
+  if (route) {
+    request.query = parsedUrl.query;
+    request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(body));
+    };
+
+    if (['POST', 'PUT', 'PATH'].includes(request.method)) {
+      bodyParser(request, () => route.handler(request, response));
+    } else {
+      route.handler(request, response);
+    }
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
+  }
+});
+
+server.listen(3000, () =>
+  console.log('Server started at: http://localhost:3000')
+);
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Eri Johnson',
+  },
+  {
+    id: 2,
+    name: 'Ramon',
+  },
+  {
+    id: 3,
+    name: 'Gleury',
+  },
+];
